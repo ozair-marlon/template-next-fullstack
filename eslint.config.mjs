9b598f03670d8e75421a9c54583d7a80e55f2b90
ozair-marlon <ozair.marlon@proton.me>
2026-02-05 06:59:12 -0300

    ci(eslint): adiciona configurações de Prettier a ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
